4b8cdd4adb2b82c51407f211acaa2da7e05bfe40
Misterixik <MisterixCZ@gmail.com>
2022-05-22 21:08:36 +0200

    Init_DbContext

---
+++ TestApiProject/DataLib/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace DataLib.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            { 
+
+            }
+    }
+}
---
+++ TestApiProject/DataLib/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using DataLib.Models;
+
+namespace DataLib
+{
+    public class Program
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase(databaseName: "NameOfDatabase"));
+        }
+}
